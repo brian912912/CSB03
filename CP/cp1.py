c63ef0b227ca957bfc812ad6e2ae4ec4e9231aec
brian912912 <hovanngoc912@gmail.com>
2024-04-21 11:37:25 +0700

    cp1

---
+++ CP/cp1.py
@@ -0,0 +1,250 @@
+#P1
+
+##b1
+# f_name = input("Nhập tên của bạn")
+# l_name = input("Nhập họ của bạn")
+# print("Your full name is ", f_name, l_name)
+
+
+
+# ##B2
+# inp = input('nhập tên  ')
+# print(inp.upper())
+
+#b3
+# a = int(input('nhập độ dài'))
+# print(float(a*a))
+
+# #b4
+# r = float(input("Input circle's radius: "))
+# import turtle
+# turtle_3 = turtle.Turtle()
+# turtle_3.circle(r)
+# turtle.mainloop()
+
+#p2
+#b1
+# for i in range(3,13):
+#     print(i)
+
+#b2
+# b = int(input('hãy nhập số'))
+# for o in range(0,b+1):
+#     print(o)
+
+#b3
+# c = int(input('hãy nhập số '))
+# for l in range(1, c+1,2):
+#     print(l)
+
+
+
+# #b4
+# p = int(input('nhập số cạnh'))
+# from turtle  import *
+# for p in range (1,p+1):
+#     forward(50)
+#     left(360/p)
+
+
+#p3
+#b1
+# n = int(input('nhập số'))
+# if n > 13:
+#     print('số này lớn hơn 13')
+# else:
+#     print('số này không lớn hơn 13')
+
+#b2
+# x = int(input('nhập số'))
+# if x%2 == 0:
+#     print('số này là số chẵn')
+# else:
+#     print('số này là số lẻ')
+
+#b3
+# m = int(input('nhập tháng'))
+# if (m<=0) or  (m >12):
+#     print('tháng không hợp lệ')
+# else:
+#     if m ==2:
+#         print('tháng này có 28 ngày')
+#     elif (m == 1) or (m == 3) or (m == 5) or (m == 7) or (m == 8) or (m == 10) or (m == 12) :
+#         print('tháng này có 31 ngày')
+#     else:
+#         print('tháng này có 30 ngày')
+
+
+#p4
+#b1
+# name = str(input("hãy nhập tên đăng nhập"))
+# passw = str(input("hãy nhập mật khẩu"))
+# email = str(input('hãy nhập email'))
+# if name != "" & passw!="" & email!="":
+#     print('đăng nhập thành công')
+# else:
+#     print('không được để trống')
+
+#b2
+# name = str(input("hãy nhập tên đăng nhập"))
+# passw = str(input("hãy nhập mật khẩu"))
+# r_passw = str(input('hãy nhập lại password'))
+# email = str(input('hãy nhập email'))
+# if name != "" & passw!="" & email!=""&r_passw!="":
+#     if passw==r_passw:
+#         print('đăng nhập thành công')
+#     else:
+#         print('hãy nhập lại pass word')
+#         passw = str(input("hãy nhập mật khẩu"))
+#         if passw==r_passw:
+#          print('đăng nhập thành công')
+#         else:
+#          print('hãy nhập lại pass word')
+# else:
+#     print('không được để trống')
+
+#b3
+# name = str(input("hãy nhập tên đăng nhập"))
+# passw = str(input("hãy nhập mật khẩu"))
+# r_passw = str(input('hãy nhập lại password'))
+# email = str(input('hãy nhập email'))
+# if "@" & "." in email:
+#    print('email hợp lệ')
+#    if name != "" & passw!="" & email!=""&r_passw!="":
+#     if passw==r_passw & len(passw) >= 8 & passw.isalnum() == True:
+#         print('đăng nhập thành công')
+#     else:
+#         print('hãy nhập lại pass word')
+#         passw = str(input("hãy nhập mật khẩu"))
+#         passw = str(input("hãy nhập mật khẩu"))
+#         if passw==r_passw & len(passw) >= 8 & passw.isalnum() == True:
+#          print('đăng nhập thành công')
+#         else:
+#          print('hãy nhập lại pass word')
+#    else:
+#         print('không được để trống')
+# else:
+#     print('email không hợp lệ')
+#     email = str(input('hãy nhập email'))
+#     if "@" & "." in email:
+#         print('email hợp lệ')
+#     else:
+#        print('email không hợp lệ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
