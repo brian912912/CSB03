f7134dbb907a0ab08869ed98d1b672f4301c3615
brian912912 <hovanngoc912@gmail.com>
2024-05-12 11:46:12 +0700

    cp2

---
+++ CP/cp2.py
@@ -0,0 +1,264 @@
+#p1
+ 
+#b1
+# L = ['blue','green','red']
+
+# #b2
+# print('color list',L)
+
+# #b3
+# q = str(input('fill a color'))
+# L.append(q)
+
+#p2
+# #b1
+# w = int(input('nhap stt '))
+# print(L[w-1])
+
+##b2
+# color = ['blue','red','teal','green']
+# posdel = input('number of position: ')
+# if posdel.isdigit()==True:
+#     x = int(posdel)
+#     # print(type(x))
+#     color.pop(x-1)
+#     print('new color list:',color)
+# else:
+#     color.remove(posdel)
+#     print('new color list:',color)
+
+
+##p3
+#numb = [5, 1, 8, 92, -1, 30]
+##b1
+# t = int(input('fil a number'))
+# if t in numb:
+#     print('number is founded')
+# else :
+#     print('number not founded')
+# number = [1,3,6,-10,9]
+# print([1])
+# count = 0
+# x = int(input('input a number: '))
+# for i in numb:
+#     if i == x:
+#         print(numb.index(i)+1)
+
+##b2
+#print(sum(numb))
+
+##b3
+# numb2 = []
+# i = 1
+# while i != 0:
+#     i = int(input('fil a number '))
+#     numb2.append(i)
+# if i ==0:
+#     print('tong so trong mang = ',sum(numb2))
+
+##p4
+##b1
+# y = [5, 1, 8, 92, 7, 30]
+# u = 0
+# while u < 6 :
+#     if y[u]%2==0:
+#         print('so chan la',y[u])
+#     u+=1
+
+##b2
+# numb3 = []
+# p = 1
+# while p != 0:
+#     p = int(input('fil a number '))
+#     numb3.append(p)
+# o = 0
+# while o < 6 :
+#     if numb3[o]%2==0:
+#         print('so chan la',numb3[o])
+#     o+=1
+
+##p7
+##b1
+#hscore =[100, 78, 65]
+
+##b2
+# a = 0
+# print( a+1,'.',hscore[a])
+# a+=1
+# print( a+1,'.',hscore[a])
+# a+=1
+# print( a+1,'.',hscore[a])
+
+##b3
+# a = 0
+# s = int(input('fil a number '))
+# hscore.append(s)
+# print( a+1,'.',hscore[a])
+# a+=1
+# print( a+1,'.',hscore[a])
+# a+=1
+# print( a+1,'.',hscore[a])
+# a+=1
+# print( a+1,'.',hscore[a])
+# a+=1
+
+##p8
+##b1
+# d = 0
+# f = int(input('nhap so '))
+# hscore.append(f)
+# while d<4:
+#     g = max(hscore)
+#     print(d+1,'.',max(hscore))
+#     hscore.remove(g)
+#     d+=1
+
+##b2
+# nhscore =[100, 78, 65, 53, 40]
+# d = 0
+# f = int(input('nhap so '))
+# nhscore.append(f)
+# while d<5:
+#     g = max(nhscore)
+#     print(d+1,'.',max(nhscore))
+#     nhscore.remove(g)
+#     d+=1
+
+##p5
+##b1
+dist = ['BĐ','BTL','CG','ĐĐ','HBT']
+pop = [247100, 333300, 266800, 420900, 318000]
+
+##b2
+
+
+
+
+h = max(pop)
+j = pop.index(h)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ baihoc/ls6.py
@@ -0,0 +1,129 @@
+# hs = ["ngọc", 'huy', 'hiếu', 'tài']
+
+# print(hs)
+# hs[1]="lam"
+# hs.append('huy')
+# print(hs)
+ 
+# so = [1,2,3,4,5]
+# hs.extend(so)
+# print(hs)
+
+# #xoa = index
+# hs.pop(0)
+
+# #xoa = value
+# hs.remove('huy')
+
+# #xoa all
+# hs.clear
+
+# #nhập vj trí thì xóa k thì xóa al
+# del hs[1]
+
+
+# x = 0
+# while x < len(hs):
+#     print(hs[x])
+#     x+=1
+
+# newL = hs + so 
+# print(newL)
+
+# #b1
+# MEnU = ['pho', 'bun bo', 'xoi','ngu coc','banh mi']
+# for i in MEnU:
+#     print(i)
+# xoa = str(input('nhập món muốn xóa'))
+# print
+# MEnU.remove(xoa)
+# for i in MEnU:
+#     print(i)
+
+
+# #b2
+# a = int(input('nhập số thứ nhất '))
+# b = int(input('nhập số thứ hai '))
+# c = int(input('nhập số thứ ba '))
+# d = int(input('nhập số thứ tư '))
+# List = [a,b,c,d]
+# print( "Tổng các số là ", sum(List))
+
+# #b3
+# x = int(input('nhập số thứ nhất '))
+# y = int(input('nhập số thứ hai '))
+# w = int(input('nhập số thứ ba '))
+# z = int(input('nhập số thứ tư '))
+# mylist = [x,y,w,z]
+# print( 'số lớn nhất :  max(mylist))
+
+#b4
+numb = []
+
+e = int(input('nhập số thứ nhất '))
+f = int(input('nhập số thứ hai '))
+g = int(input('nhập số thứ ba '))
+h = int(input('nhập số thứ tư '))
+num = [e,f,g,h]
+i = min(num)
+num.remove(i)
+j = min(num)
+num.remove(j)
+k = min(num)
+num.remove(k)
+l = min(num)
+number = [i,j,k,l]
+print(number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
